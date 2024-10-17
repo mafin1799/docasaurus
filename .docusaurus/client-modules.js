9b80394f38beac94b7f6adf97d9e632d622a41f8
mafin <nik.chujkomail.ru>
2024-10-17 21:16:00 +0300

    Deploy website - based on fc2a07d7a84dbc9aecb20e1ca5484fa46b7858aa

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\Никита\\Documents\\docasaurus\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Никита\\Documents\\docasaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Никита\\Documents\\docasaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Никита\\Documents\\docasaurus\\src\\css\\custom.css"),
-];
