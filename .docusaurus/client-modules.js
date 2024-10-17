7af66f1ebcbc0a288a0d3d1c0b868c871423bba2
mafin <nik.chujkomail.ru>
2024-10-17 21:10:03 +0300

    .

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\Никита\\Documents\\docasaurus\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\Никита\\Documents\\docasaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\Никита\\Documents\\docasaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\Никита\\Documents\\docasaurus\\src\\css\\custom.css"),
+];
